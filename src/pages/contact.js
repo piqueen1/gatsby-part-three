b725bb5b8c7bd6317305edc3223745a76794a6fb
Finn Thye <piqueen@gmail.com>
2020-06-26 07:55:30 -0600

    apply layout to about, contact and index

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,13 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default function Contact() {
   return (
-    <div>
+    <Layout>
       <h1>I'd love to talk! Email me at the address below</h1>
       <p>
         <a href="mailto:piqueen@gmail.com">piqueen@gmail.com</a>
       </p>
-    </div>
+    </Layout>
   )
 }
